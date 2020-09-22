6ac7f278b4f8049174374f6a75497a3fbd9aa6e6
zhangdecheng <17865815379@163.com>
2020-09-22 21:35:41 +0800

    添加商品列表页

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+import './assets/reset.css'
 
 new Vue({
   router,
---
+++ src/views/Z-goodsList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <header>< 商品列表 - 网易严选</header>
+    <main></main>
+    <footer></footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
